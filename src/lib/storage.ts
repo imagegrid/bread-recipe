750f0c6d9871161fb3437ee2e8e5e788ab0a10d4
imagegrid <georgebutler@gmail.com>
2021-06-15 23:00:53 -0300

    comments

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,12 +1,15 @@
 function get(name: string, defaultValue: any): any {
     const item = localStorage.getItem(name);
     if (item) {
+        // Integer
         if (Number.isInteger(parseInt(item))) {
             return parseInt(localStorage.getItem(name));
         }
+        // Boolean
         if (item === 'true' || item === 'false') {
             return localStorage.getItem(name) === 'true';
         }
+        // String
         return localStorage.getItem(name);
     }
     return defaultValue;
