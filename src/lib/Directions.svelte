77b29080090d074beb6751c626eb6f8a85e5c271
imagegrid <georgebutler@gmail.com>
2021-08-31 22:23:41 -0300

    change knead time

--- src/lib/Directions.svelte
+++ src/lib/Directions.svelte
@@ -9,7 +9,7 @@
         <ol>
             <li>Mix warm water (38°C/100°F) and yeast.</li>
             <li>Add flour, salt, shortening.</li>
-            <li>Knead 20 mins, or until smooth ball.</li>
+            <li>Knead 10 mins, or until smooth ball.</li>
             <li>Cover the bowl with a towel and place in oven with light on for 45 mins.</li>
             <li>Remove from oven, lightly press out large air bubbles, form into loafs, and place in greased bread trays.</li>
             <li>Let the dough rise for an hour.</li>
