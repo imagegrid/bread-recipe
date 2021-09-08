3817900e0db1bf183aaf3bc6abe3d54328940487
imagegrid <georgebutler@gmail.com>
2021-09-08 11:35:35 -0300

    remove console logs

--- src/lib/Ingredients.svelte
+++ src/lib/Ingredients.svelte
@@ -35,10 +35,7 @@
     function convertToCups(grams: number, gramsPerCup: number): string {
         let fraction = '';
         const num = (Math.round((grams / gramsPerCup) * 4) / 4).toFixed(2);
-        console.log(num);
-
         const c = parseFloat(num);
-        console.log(c);
 
         if (c % 1 === 0.25) fraction = '¼';
         if (c % 1 === 0.5) fraction = '½';
